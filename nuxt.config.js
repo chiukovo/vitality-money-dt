f2279da2ef23f9e892b3f71f320556b7c7526d98
chiuko <chiuko@celloes.com>
2019-12-16 11:59:14 +0800

    fix

---
+++ nuxt.config.js
@@ -0,0 +1,89 @@
+require('dotenv').config()
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+
+module.exports = {
+  mode: 'universal',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' }
+    ],
+    link: [
+    ],
+    script: [
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: false,
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/sass/default.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~plugins/global',
+    {src: '~plugins/websocket.js', ssr: false},
+    {src: '~plugins/auth.js', ssr: false},
+    {src: '~plugins/element-ui', ssr: true},
+    {src: '~plugins/vue-table', ssr: false},
+    '@/static/js/el-dialogdrag'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/dotenv',
+    '@nuxtjs/axios',
+    '@nuxtjs/device',
+    '@nuxtjs/proxy',
+    '@nuxtjs/style-resources',
+    'cookie-universal-nuxt',
+    ['nuxt-vuex-localstorage', {
+      sessionStorage: ['sessionStorage'],
+      localStorage: ['localStorage'],
+    }]
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    transpile: [/^element-ui/],
+    extend(config, ctx) {
+      config.plugins.unshift(new LodashModuleReplacementPlugin)
+      config.module.rules[2].use[0].options.plugins = ['lodash']
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  },
+  proxy: {
+    '/api': {
+      target: process.env.NUXT_ENV_API_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api' : ''
+        }
+      }
+  },
+  router: {
+  },
+  chainWebpack: config => {
+    //close eslint
+    config.module.rules.delete("eslint");
+  }
+}
