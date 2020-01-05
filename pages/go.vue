884cf1df691a1cfd41457725a7fe608ff31c25fa
Your Name <you@example.com>
2020-01-05 22:24:59 +0800

    add go page

---
+++ pages/go.vue
@@ -0,0 +1,34 @@
+<template lang='pug'>
+div
+</template>
+
+<script>
+
+export default {
+	data() {
+	  return {
+
+	  }
+	},
+	mounted() {
+		const {
+			UserID,
+			UserToken,
+			ReturnURL,
+		} = this.$route.query;
+		
+		if (UserID && UserToken && ReturnURL) {
+			this.$store.commit("setMobile", 0)
+			//set user info
+			console.log(UserID);
+			this.$store.commit('setuserAuth', {
+				UserId: UserID,
+				Token: UserToken,
+				ReturnURL: ReturnURL,
+			})
+		}
+	},
+	methods: {
+	},
+}
+</script>
---
+++ pages/mobile/go.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+div
+</template>
+
+<script>
+
+export default {
+	data() {
+	  return {
+
+	  }
+	},
+	mounted() {
+		const {
+			UserID,
+			UserToken,
+			ReturnURL,
+		} = this.$route.query
+
+		if (UserID && UserToken && ReturnURL) {
+			this.$store.commit("setMobile", 1)
+			//set user info
+			this.$store.commit('setuserAuth', {
+				UserId: UserID,
+				Token: UserToken,
+				ReturnURL: ReturnURL,
+			})
+		}
+	},
+	methods: {
+	},
+}
+</script>
