14b8528916798052ddc43c3980b70c4b4c061309
Your Name <you@example.com>
2020-03-06 22:02:56 +0800

    fix reconnect

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
     script: [
       { src: '/js/charting_library/charting_library.min.js', type: 'text/javascript' },
+      { src: '/js/reconnecting-websocket.min.js', type: 'text/javascript' },
     ]
   },
   /*
